98c6399e842185e31500f8115bd05ba565736ebf
Ryan Sinnott <ryansinnott58@gmail.com>
2020-01-20 18:08:58 -0500

    Removed some code

--- script.py
+++ script.py
@@ -1,20 +1,21 @@
-import math
-import os
-import sys
+# import math
+# import os
+# import sys
 
 import requests
 
-print(sys.version)
-print(sys.executable)
+# print(sys.version)
+# print(sys.executable)
 
 
-def greet(who_to_greet):
-    greeting = "Hello, {}".format(who_to_greet)
-    return greeting
+# def greet(who_to_greet):
+#     greeting = "Hello, {}".format(who_to_greet)
+#     return greeting
 
 
 r = requests.get("https://ryansinnott.com")
 print(r.status_code)
+print(r.ok)
 
 # name = input("Your name? ")
 # print("Hello,", name)
